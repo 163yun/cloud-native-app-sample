f2918590b8d53af08473b1895188023a0ad0f598
bingohuang <me@bingohuang.com>
2016-12-07 15:07:06 +0800

    init project

---
+++ native-cloud-app-1/src/main/java/com/bingohuang/NativeCloudApplication.java
@@ -0,0 +1,17 @@
+package com.bingohuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: bingohuang
+ * Date: 07/12/2016
+ * Time: 13:48
+ */
+@SpringBootApplication
+public class NativeCloudApplication {
+    public static void main(String[] args)  {
+        SpringApplication.run(NativeCloudApplication.class, args);
+    }
+
+}
